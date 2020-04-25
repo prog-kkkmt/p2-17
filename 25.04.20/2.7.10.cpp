092873da76491bef2fa819f5d3873bf57a6751dc
GoldGoblin-pascal <63998165+GoldGoblin-pascal@users.noreply.github.com>
2020-04-25 16:28:53 +0300

    Add files via upload

---
+++ 25.04.20/2.7.10.cpp
@@ -0,0 +1,16 @@
+void swap_min(int *m[], unsigned rows, unsigned cols)
+{
+    int buf, min = m[0][0];
+
+    for (int i = 0; i<rows; i++)
+        for (int j = 0; j<cols; j++)
+            if (m[i][j] < min)
+            {
+                buf = i;
+                min = m[i][j];
+            }
+    int *buf1 = m[buf];
+    m[buf] = m[0];
+    m[0] = buf1;
+
+}
