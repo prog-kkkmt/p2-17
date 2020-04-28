c5d35f06085b2977e89cb4b2cb223dc40c9a3c91
Leonid Filippov <ski20173@yandex.ru>
2020-04-28 14:03:59 +0300

    Added codes for 4.1

---
+++ codes/4.1.9.cpp
@@ -0,0 +1,29 @@
+/* Этот код просто напоминание как выглядит класс Foo и функция foo_says.
+ * Не нужно его расскоментировать и/или менять.
+ *
+ * #include <iostream>
+ *        
+ * struct Foo {
+ *     void say() const { std::cout << "Foo says: " << msg << "\n"; }
+ * protected:
+ *     Foo(const char *msg) : msg(msg) { }
+ * private:
+ *     const char *msg;
+ * };
+ * 
+ * void foo_says(const Foo &foo) { foo.say(); }
+ *
+ */
+
+// Вам нужно определить функцию get_foo, как описано в задании,
+// чтобы компилировался и работал как ожидается следующий код:
+//
+// foo_says(get_foo("Hello!"));
+
+Foo get_foo(const char *msg) {
+    struct Child_Foo : Foo {
+        public:
+        Child_Foo(const char *msg) : Foo(msg) { }
+    };
+    return Child_Foo(msg);
+}
