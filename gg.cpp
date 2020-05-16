854c4b700841131efd96809aec2ebdf5f0be78d4
Streel <63997593+Streels@users.noreply.github.com>
2020-05-16 16:51:37 +0300

    Add files via upload

---
+++ gg.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+struct str
+{
+    size_t len;
+    char *a = new char[len];
+};
+
+int main()
+{
+    vector<str>v;
+    str tempV;
+
+    cin >> *tempV.a;
+    v.push_back(tempV);
+
+    cin >> *tempV.a;
+    v.push_back(tempV);
+
+    cin >> *tempV.a;
+    v.push_back(tempV);
+
+    cout << v[0].a << v[1].a << v[2].a;
+
+    return 0;
+}
