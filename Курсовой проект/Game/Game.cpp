386057c69bcaa1783b5c6e7af4dda3544524d1b0
Streel <63997593+Streels@users.noreply.github.com>
2020-06-20 14:49:14 +0300

    Add files via upload

---
+++ Курсовой проект/Game/Game.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <cstring>
+#include <sstream>
+#include <SFML/Graphics.hpp>
+
+#include "resourse/MainMenu.hpp"
+
+using namespace std;
+using namespace sf;
+
+int main()
+{
+	//Вызов фунции лаунчера
+	launcher(); 
+	return 0;
+}
