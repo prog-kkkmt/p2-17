89cde245a9958aa317aa7c49feddf5824daa6119
mloborev <39646455+mloborev@users.noreply.github.com>
2020-06-22 13:46:59 +0300

    Update Course_work.cpp

--- Курсовой проект/Исходный код/Course_work.cpp
+++ Курсовой проект/Исходный код/Course_work.cpp
@@ -6,28 +6,29 @@
 
 int main()
 {
-    system("chcp 1251");
+	system("chcp 1251");
 	system("cls");
-    std::cout << "Выберите метод шифрования:" << std::endl;
-    std::cout << "1. Шифр Цезаря." << std::endl;
+	std::cout << "Выберите метод шифрования:" << std::endl;
+	std::cout << "1. Шифр Цезаря." << std::endl;
 	std::cout << "2. Морзе." << std::endl;
 	std::cout << "3. Выход из программы." << std::endl;
-    std::cout << "Ответ: ";
+	std::cout << "Ответ: ";
 
-	Sipher oh_overlord;
+	Sipher mloborev;
 	
-    int choice;
-    std::cin >> choice;
-    switch (choice)
-    {
-    case 1:
-		oh_overlord.CaesarEncryption(); break;
-	case 2:
-		oh_overlord.MorseEncryption();  break;
-    case 3:
-        exit(0);
+	int choice;
+	std::cin >> choice;
+	switch (choice)
+	{
+		case 1:
+			mloborev.CaesarEncryption(); break;
+		case 2:
+			mloborev.MorseEncryption();  break;
+		case 3:
+			exit(0);
     }
-
-    std::cout << std::endl;
-    system("pause");
+	
+	std::cout << std::endl;
+	system("pause");
+	exit(0);
 }
