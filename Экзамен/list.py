ced47bf170ae8b7797eea3ee51eac314bd474b16
Anatoliy <64305034+Velligan@users.noreply.github.com>
2020-06-29 11:13:57 +0300

    Create list.py

---
+++ Экзамен/list.py
@@ -0,0 +1,17 @@
+# Последовательности в языка программирования.
+# П2 - 17 Швыркин А.П.
+from random import randint
+
+N = 10
+a = []
+for i in range(N):
+    a.append(randint(1, 99))
+print(a)
+
+
+for i in range(N-1):
+    for j in range(N-i-1):
+        if a[j] > a[j+1]:
+            a[j], a[j+1] = a[j+1], a[j]
+
+print(a)
