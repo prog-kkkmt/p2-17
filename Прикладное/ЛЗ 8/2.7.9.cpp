27360960e2bdcb5697b4c0ec1235f539a58cb440
DaniilF-R <64002839+DaniilF-R@users.noreply.github.com>
2020-09-09 21:23:49 +0300

    Add files via upload

---
+++ Прикладное/ЛЗ 8/2.7.9.cpp
@@ -0,0 +1,16 @@
+int ** transpose(const int * const * m, unsigned rows, unsigned cols)
+{
+int **matrix = new int *[cols];
+
+for (int i = 0; i<cols; i++)
+matrix[i] = new int [rows];
+
+for (int i=0; i<cols; i++)
+{
+for (int j=0; j<rows; j++)
+{
+matrix[i][j] = m[j][i];
+}
+}
+return(matrix);
+}
