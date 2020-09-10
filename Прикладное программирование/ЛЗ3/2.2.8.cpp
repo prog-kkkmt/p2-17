3178441d520e9c45e5a1357ddaad739560670dad
GrishaevAndrej <66481719+GrishaevAndrej@users.noreply.github.com>
2020-09-10 20:37:35 +0300

    Update 2.2.8.cpp

--- Прикладное программирование/ЛЗ3/2.2.8.cpp
+++ Прикладное программирование/ЛЗ3/2.2.8.cpp
@@ -1,3 +1,4 @@
+//Напишите рекурсивную функцию вычисления наибольшего общего делителя двух положительных целых чисел (Greatest Common Divisor, GCD). 
 unsigned gcd(unsigned a, unsigned b)
 {
 if(b==0)
