1b9c96caa3934beef0905b3137ae7ac4b96ca157
juk1359 <64369746+juk1359@users.noreply.github.com>
2020-09-13 21:09:42 +0300

    Add files via upload

---
+++ Прикладное/ЛЗ-4/2.3.9.cpp
@@ -0,0 +1,15 @@
+//Нужно реализовать функцию, которая сдвигает содержимое массива влево на заданное число позиций.
+
+
+void rotate(int a[], unsigned size, int shift)
+{
+for (int i=0; i<shift; i++)
+{
+int buf = a[0];
+for (int j=0; j<size-1; j++)
+{
+a[j] = a[j+1];
+}
+a[size-1] = buf;
+}
+}
