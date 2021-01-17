99fffa4ef54695187877fd5d5bc30516d0eb93d2
GrishaevAndrej <66481719+GrishaevAndrej@users.noreply.github.com>
2021-01-17 18:56:02 +0300

    Add files via upload

---
+++ Прикладное/Лекция 11. Конструкторы, деструкторы, объекты/lekcija11.c
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+class Cpoint
+{
+	private:
+		int *x; //поле x
+		int *y; //поле y
+	public:
+		Cpoint(int x = 0, int y = 0); //прототип конструктора
+		void out();
+		~Cpoint(); //прототип деструктора
+
+};
+
+Cpoint :: Cpoint(int _x, int _y) //выдел память для хранения координат точки
+		{
+			x = new int;
+			*x = _x;
+			y = new int;
+			*y = _y;
+		}
+
+void Cpoint :: out()
+{
+	cout << "Segment : (" << *x << "," << *y << ")" << endl;
+}
+Cpoint :: ~Cpoint() //освобожд память
+{
+	delete x;
+	delete y;
+}
+
+int main()
+{
+	Cpoint A(3, 4), B;
+	A.out();
+	B.out();
+
+	return 0;
+}
